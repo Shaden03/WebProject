3d160bba3261e102b23405a0fd4b63003145f952
EprojectSWE <atheer31.2001@gmail.com>
2024-05-18 02:04:17 +0300

    Update port, sin in

--- app.js
+++ app.js
@@ -1,47 +1,52 @@
-var express=require("express")
-var bodyParser=require("body-parser")
-var mongoose=require("mongoose")
+var express = require("express");
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
 
-const app=express()
+const app = express();
 
-app.use(bodyParser.json())
-app.use(express.static('public'))
+app.use(bodyParser.json());
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({
-    extended:true
-}))
+    extended: true
+}));
 
-mongoose.connect('mongodb://localhost:27017/Database')
-var db=mongoose.connection
-db.on('error',()=> console.log("Error in Connecting to Database"))
-db.once('open',()=> console.log("Connected to Database"))
+mongoose.connect('mongodb://localhost:27017/Database', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
-app.post("/sign_up",(req,res) => {
 
-    var username= req.body.username
-    var email=req.body.email
-    var password=req.body.password
+app.post("/sign_up", (req, res) => {
+    var name = req.body.name;
+    var age = req.body.age;
+    var email = req.body.email;
+    var phno = req.body.phno;
+    var gender = req.body.gender;
+    var password = req.body.password;
 
-    var data={
-        "username":username,
-        "email":email,
-        "password":password
-    }
-    db.collection('users').insertOne(data,(err,collection) => {
-        if(err){
+    var data = {
+        "name": name,
+        "age": age,
+        "email": email,
+        "phno": phno,
+        "gender": gender,
+        "password": password
+    };
+    db.collection('users').insertOne(data, (err, collection) => {
+        if (err) {
             throw err;
         }
-        console.log("Record Inserted Succesfully")
-    })
-    return res.redirect('index.html')
+        console.log("Record Inserted Successfully");
+    });
+    return res.redirect('signup_successful.html');
+});
 
-})
-
-app.get('/', (req, res)=> {
+app.get("/", (req, res) => {
     res.set({
-        "Allow-acces-Allow-Origin":'*'
-    })
-    return res.redirect('signup.html')
+        "Allow-acces-Allow-Origin": '*'
+    });
+    return res.redirect('signup.html');
+});
 
-   }).listen(3000);
-   
-   console.log(`listen on port 3000`)
+app.listen(3000);
+console.log("Listening on port 3000");
